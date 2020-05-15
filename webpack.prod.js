9b63a913aa483d94d457dac5afff8e03cc0a25ba
Shadab Ali <shadabali604@gmail.com>
2020-05-15 14:55:43 +0530

    project improvement done

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,45 +1,43 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-  entry: './src/client/index.js',
-  output: {
-    libraryTarget: 'var',
-    library: 'Client',
-  },
+  entry: './src/public/js/index.js',
   mode: 'development',
-  devtool: 'source-map',
-  stats: 'verbose',
   module: {
     rules: [
       {
-        test: '/.js$/',
+        test: '/\.js$/',
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
-    ],
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(jpg|png|svg|jpg|gif)$/,
+        loader: 'file-loader'
+      }
+    ]
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/client/views/index.html',
-      filename: './index.html',
+    new HTMLWebPackPlugin({
+      template: './src/public/index.html',
+      filename: 'index.html'
     }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
-    }),
-    // new WorkboxPlugin.GenerateSW(),
-  ],
-};
+    new WorkboxPlugin.GenerateSW()
+  ]
+}
